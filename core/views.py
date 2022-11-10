652e1d8c8f8d57866a48ed41a54cb27a0a4dd2a1
sonlv69 <kiluss2@gmail.com>
2022-11-10 17:57:08 +0700

    push

--- core/views.py
+++ core/views.py
@@ -1,9 +1,10 @@
 
 from django.http import HttpResponse
 import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
+from firebase_admin import credentials, firestore, messaging
 import json
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 cred = credentials.Certificate("mysite/serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
@@ -11,12 +12,45 @@ db = firestore.client()
 users_ref = db.collection('shop_clones')
 
 
-
 def index(request):
-        #accessing our firebase data and storing it in a variable
-        docs = users_ref.stream()
-        list = []
-        for doc in docs:
-            list.append(doc.to_dict())
+    #accessing our firebase data and storing it in a variable
+    docs = users_ref.stream()
+    list = []
+    for doc in docs:
+        list.append(doc.to_dict())
+    return HttpResponse(json.dumps(list))
 
-        return HttpResponse(json.dumps(list))
+@api_view(['POST'])
+def sendNotification(request):
+    # This registration token comes from the client FCM SDKs.
+    registration_tokens = []
+    for key in request.data:
+        registration_tokens.append(key)
+        print(key)
+
+    # See documentation on defining a message payload.
+    message = messaging.MulticastMessage(
+        notification=messaging.Notification('Price drop', '5% off all electronics'),
+        tokens=registration_tokens,
+    )
+
+    # Send a message to the device corresponding to the provided
+    # registration token.
+    response = messaging.send_multicast(message)
+    # Response is a message ID string.
+    print('Successfully sent message:', response)
+    return Response(response.status_code)
+
+
+def subscribe_news(tokens): # tokens is a list of registration tokens
+ topic = 'news'
+ response = messaging.subscribe_to_topic(tokens, topic)
+ if response.failure_count > 0:
+  print(f'Failed to subscribe to topic {topic} due to {list(map(lambda e: e.reason,response.errors))}')
+
+
+def unsubscribe_news(tokens): # tokens is a list of registration tokens
+ topic = 'news'
+ response = messaging.unsubscribe_from_topic(tokens, topic)
+ if response.failure_count > 0:
+  print(f'Failed to unsubscribe to topic {topic} due to {list(map(lambda e: e.reason,response.errors))}')
